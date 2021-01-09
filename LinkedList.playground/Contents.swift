c9bc5fdb75b6b2d7cd93ce48874808d5e26eb750
OddMagnet <mibruenen@gmail.com>
2021-01-09 18:10:13 +0100

    Same, but as a struct with enum nodes

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,33 +1,41 @@
 import Foundation
 
-class LinkedListNode<Element> {
-    var value: Element
-    var next: LinkedListNode?
+// indirect makes it so that the enum cases are effectively reference types
+indirect enum LinkedListNode<Element> {
+    case node(value: Element, next: LinkedListNode<Element>?)
 
-    init(value: Element, next: LinkedListNode<Element>? = nil) {
-        self.value = value
-        self.next = next
+    var value: Element? {
+        switch self {
+            case .node(let value, _):
+                return value
+        }
+    }
+    var next: LinkedListNode<Element>? {
+        switch self {
+            case .node(_, let next):
+                return next
+        }
     }
 }
 
-class LinkedList<Element>: ExpressibleByArrayLiteral, Sequence {
+struct LinkedList<Element>: ExpressibleByArrayLiteral, Sequence {
     var start: LinkedListNode<Element>?
 
     init(start: LinkedListNode<Element>) {
         self.start = start
     }
 
-    required public init(arrayLiteral elements: Element...) {
+    public init(arrayLiteral elements: Element...) {
         for element in elements.reversed() {
             // create list in reverse, the last elements `next` points to nil,
             // the second to last will point to the last and so on
-            start = LinkedListNode(value: element, next: start)
+            start = LinkedListNode.node(value: element, next: start)
         }
     }
 
     init(array: Array<Element>) {
         for element in array.reversed() {
-            start = LinkedListNode(value: element, next: start)
+            start = LinkedListNode.node(value: element, next: start)
         }
     }
 
@@ -44,4 +52,15 @@ struct LinkedListIterator<Element>: IteratorProtocol {
         defer { current = current?.next }
         return current
     }
+}
+
+
+var third = LinkedListNode.node(value: 5, next: nil)
+var second = LinkedListNode.node(value: 3, next: third)
+var first = LinkedListNode.node(value: 1, next: second)
+
+let list = LinkedList(start: first)
+
+for node in list {
+    print(node.value ?? "n/a")
 }
