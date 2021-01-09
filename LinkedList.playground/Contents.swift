1bb2ca0dc41a56b9edbe3f1b948f29ef3614941c
OddMagnet <mibruenen@gmail.com>
2021-01-09 17:43:03 +0100

    Add ExpressibleByArrayLiteral

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -4,12 +4,20 @@ class LinkedListNode<Element> {
     var value: Element
     var next: LinkedListNode?
 
-    init(value: Element) {
+    init(value: Element, next: LinkedListNode<Element>? = nil) {
         self.value = value
-        self.next = nil
+        self.next = next
     }
 }
 
-class LinkedList<Element> {
+class LinkedList<Element>: ExpressibleByArrayLiteral {
     var start: LinkedListNode<Element>?
+
+    required public init(arrayLiteral elements: Element...) {
+        for element in elements.reversed() {
+            // create list in reverse, the last elements `next` points to nil,
+            // the second to last will point to the last and so on
+            start = LinkedListNode(value: element, next: start)
+        }
+    }
 }
