5a27a0c8255dd7d146a38117e62b34688e53fee6
OddMagnet <mibruenen@gmail.com>
2021-01-09 17:36:41 +0100

    Initial LinkedList

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,2 +1,15 @@
 import Foundation
 
+class LinkedListNode<Element> {
+    var value: Element
+    var next: LinkedListNode?
+
+    init(value: Element) {
+        self.value = value
+        self.next = nil
+    }
+}
+
+class LinkedList<Element> {
+    var start: LinkedListNode<Element>?
+}
