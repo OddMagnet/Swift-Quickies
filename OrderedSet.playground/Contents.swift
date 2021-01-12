c2805a86f3cc485fdee7fb1b4ed800e5ea78af80
OddMagnet <mibruenen@gmail.com>
2021-01-12 11:22:37 +0100

    Add count, isEmpty and contains()

--- OrderedSet.playground/Contents.swift
+++ OrderedSet.playground/Contents.swift
@@ -3,6 +3,8 @@ import Foundation
 struct OrderedSet<Element: Hashable>: Equatable {
     public private(set) var array = [Element]()
     private var set = Set<Element>()
+    var count: Int { array.count }
+    var isEmpty: Bool { array.isEmpty }
 
     init() { }
 
@@ -16,7 +18,7 @@ struct OrderedSet<Element: Hashable>: Equatable {
     /// - Parameter newElement: The element to append
     /// - Returns: True if the element was appended, false if the set already contains it, the result is discardable
     @discardableResult
-    mutating func append(_ newElement: Element) -> Bool {   // using 'newElement' as a parameter name since Set's append function does so as well
+    mutating func append(_ newElement: Element) -> Bool {   // using 'newElement' as a parameter name since Set's insert and Arrays's append function do so as well
         // insert into set, check the returned tuple's `inserted` part
         if set.insert(newElement).inserted {
             array.append(newElement)
@@ -24,5 +26,9 @@ struct OrderedSet<Element: Hashable>: Equatable {
         } else {
             return false
         }
+    }
+
+    func contains(_ member: Element) -> Bool {              // using 'member' as a parameter name since Set's contains function does so as well
+        set.contains(member)
     }
 }
