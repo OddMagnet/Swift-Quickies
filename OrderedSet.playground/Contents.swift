b449b48a7396e3dafdbe499931fac9c0e7deb37a
OddMagnet <mibruenen@gmail.com>
2021-01-12 11:31:59 +0100

    Update comments, extend to conform to RandomAccessCollection

--- OrderedSet.playground/Contents.swift
+++ OrderedSet.playground/Contents.swift
@@ -18,7 +18,7 @@ struct OrderedSet<Element: Hashable>: Equatable {
     /// - Parameter newElement: The element to append
     /// - Returns: True if the element was appended, false if the set already contains it, the result is discardable
     @discardableResult
-    mutating func append(_ newElement: Element) -> Bool {   // using 'newElement' as a parameter name since Set's insert and Arrays's append function do so as well
+    mutating func append(_ newElement: Element) -> Bool {   // using 'newElement' as a parameter name for consistency with Set's insert and Arrays's append function
         // insert into set, check the returned tuple's `inserted` part
         if set.insert(newElement).inserted {
             array.append(newElement)
@@ -31,12 +31,21 @@ struct OrderedSet<Element: Hashable>: Equatable {
     /// Checks if the ordered set contains a member
     /// - Parameter member: The member to check for
     /// - Returns: True if it contains the member, false if not
-    func contains(_ member: Element) -> Bool {              // using 'member' as a parameter name since Set's contains function does so as well
+    func contains(_ member: Element) -> Bool {              // using 'member' as a parameter name for consistency with Set's contains function
         set.contains(member)
     }
 
     /// Override the synthesized `==` method so Swift only compared the arrays and not the sets as well
     static func ==(lhs: OrderedSet, rhs: OrderedSet) -> Bool {
         return lhs.array == rhs.array
+    }
+}
+
+extension OrderedSet: RandomAccessCollection {
+    var startIndex: Int { array.startIndex }
+    var endIndex: Int { array.endIndex }
+
+    subscript(index: Int) -> Element {
+        array[index]
     }
 }
