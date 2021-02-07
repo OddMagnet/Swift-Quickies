20bebcc3cce423b75133e940a51304d492379061
OddMagnet <mibruenen@gmail.com>
2021-02-07 17:44:50 +0100

    Add states and color/size changes to buttons

--- Lockscreen/Lockscreen/LockscreenButton.swift
+++ Lockscreen/Lockscreen/LockscreenButton.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct LockscreenButton: View {
+    @State private var pressed = false
+    @State private var activated = false
     let image: String
+    let generator = UIImpactFeedbackGenerator()
 
     var body: some View {
         Image(systemName: image)
-            .foregroundColor(.white)
+            // Color changes based on activation state
+            .foregroundColor(
+                activated
+                    ? Color.black
+                    : Color.white
+            )
             .font(.title3)
             .frame(width: 50, height: 50)
-            .background(Color.black.opacity(0.4))
+            // Background changes based on activation and pressed state
+            .background(
+                activated
+                    ? Color.white
+                    : Color.black.opacity(
+                        pressed
+                            ? 0.8
+                            : 0.4
+                    )
+            )
             .clipShape(Circle())
+            // Scale changes based on pressed state
+            .scaleEffect(
+                pressed
+                    ? 1.5
+                    : 1
+            )
+            .animation(.spring(response: 0.5, dampingFraction: 0.7))
+            .onLongPressGesture(minimumDuration: 0.4) { bool in
+                pressed = bool
+            } perform: {
+                generator.impactOccurred()
+                activated.toggle()
+                pressed = false
+            }
     }
 }
 
