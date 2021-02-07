70987e30b2246a66e2269a1023b15f78e238f13a
OddMagnet <mibruenen@gmail.com>
2021-02-07 17:20:27 +0100

    Rough draft

--- Lockscreen/Lockscreen/ContentView.swift
+++ Lockscreen/Lockscreen/ContentView.swift
@@ -11,12 +11,37 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             GeometryReader { geo in
+                // MARK: - Background
                 Image("bigsur")
                     .resizable()
                     .scaledToFill()
                     .frame(maxWidth: geo.size.width)
 
                 Color.black.opacity(0.15)
+
+                // MARK: - Content
+                VStack(spacing: 0) {
+                    Image(systemName: "lock.fill")
+                        .font(.largeTitle)
+                        .padding(.top, 60)
+
+                    Text("9:41")
+                        .font(.system(size: 92, weight: .thin))
+
+                    Text("February 7, 2021")
+                        .font(.title2)
+
+                    Spacer()
+
+                    HStack {
+                        Spacer()
+                    }
+
+                    Capsule()
+                        .fill(Color.white)
+                        .frame(width: 150, height: 5)
+                        .padding(.bottom, 10)
+                }
             }
             .edgesIgnoringSafeArea(.all)
         }
