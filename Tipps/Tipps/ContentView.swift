ca5d96d0be85b2938852166d0d678597458fc0e7
OddMagnet <mibruenen@gmail.com>
2021-02-13 16:18:27 +0100

    Initial Tipps app commit

---
+++ Tipps/Tipps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tipps
+//
+//  Created by Michael Brünen on 13.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tipps/Tipps/TippsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippsApp.swift
+//  Tipps
+//
+//  Created by Michael Brünen on 13.02.21.
+//
+
+import SwiftUI
+
+@main
+struct TippsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
