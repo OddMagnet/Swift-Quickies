9f3efa624844be94c1dbaae399aa7c38f16f2ea6
OddMagnet <mibruenen@gmail.com>
2021-03-21 14:17:02 +0100

    Version for non-comparable elements and extension for comparable elements

--- SortedArrays.playground/Contents.swift
+++ SortedArrays.playground/Contents.swift
@@ -1,12 +1,17 @@
 import Foundation
 
-struct SortedArray<Element>: CustomStringConvertible, RandomAccessCollection where Element: Comparable {
+struct SortedArray<Element>: CustomStringConvertible, RandomAccessCollection {
     private var items = [Element]()
+    var sortBefore: (Element, Element) -> Bool
 
     var count: Int { items.count }
     var description: String { items.description }
     var startIndex: Int { items.startIndex }
     var endIndex: Int { items.endIndex }
+
+    init(comparator: @escaping (Element, Element) -> Bool) {
+        self.sortBefore = comparator
+    }
 
     subscript(index: Int) -> Element {
         items[index]
@@ -14,7 +19,7 @@ struct SortedArray<Element>: CustomStringConvertible, RandomAccessCollection whe
 
     mutating func insert(_ element: Element) {
         for (i, item) in items.enumerated() {
-            if item >= element {
+            if sortBefore(element, item) {
                 items.insert(element, at: i)
                 return
             }
@@ -24,6 +29,12 @@ struct SortedArray<Element>: CustomStringConvertible, RandomAccessCollection whe
 
     mutating func remove(at index: Int) {
         items.remove(at: index)
+    }
+}
+
+extension SortedArray where Element: Comparable {
+    init() {
+        self.init(comparator: <)
     }
 }
 
