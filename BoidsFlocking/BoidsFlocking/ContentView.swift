50966be593f11c9e29472031dae33d40571875f3
OddMagnet <mibruenen@gmail.com>
2021-04-02 16:40:05 +0200

    Finish initial ContentView

--- BoidsFlocking/BoidsFlocking/ContentView.swift
+++ BoidsFlocking/BoidsFlocking/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Just for preview purposes the flocks size is set to the dimensions of the 12 pro max (@3x)
+    @StateObject var flock = Flock(width: 428, height: 926)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ForEach(flock.boids) { boid in
+                Triangle()
+                    .rotation(.radians(boid.velocity.heading + (.pi / 2)))
+                    .fill(Color.red)
+                    .frame(width: 6, height: 12)
+                    .position(x: boid.position.x, y: boid.position.y)
+            }
+        }
+        .background(Color(white: 0.2, opacity: 1))
+        .frame(width: flock.width, height: flock.height)
+        .ignoresSafeArea()
     }
 }
 
