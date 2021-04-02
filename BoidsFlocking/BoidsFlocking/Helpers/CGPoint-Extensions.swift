8273b0a7ee7b880411d9257a7a6a9a5d7ec4b196
OddMagnet <mibruenen@gmail.com>
2021-04-02 16:49:54 +0200

    Add calculation helpers

--- BoidsFlocking/BoidsFlocking/Helpers/CGPoint-Extensions.swift
+++ BoidsFlocking/BoidsFlocking/Helpers/CGPoint-Extensions.swift
@@ -12,4 +12,37 @@ extension CGPoint {
     var heading: Double {
         Double(-atan2(-y, x))
     }
+
+    // MARK: - Calculation helpers
+    static func *=(lhs: inout CGPoint, rhs: CGFloat) {
+        lhs.x *= rhs
+        lhs.y *= rhs
+    }
+
+    static func /=(lhs: inout CGPoint, rhs: CGFloat) {
+        lhs.x /= rhs
+        lhs.y /= rhs
+    }
+
+    static func +=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs.x += rhs.x
+        lhs.y += rhs.y
+    }
+
+    static func -=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs.x -= rhs.x
+        lhs.y -= rhs.y
+    }
+
+    static func *(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
 }
