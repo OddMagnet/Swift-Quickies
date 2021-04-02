55ec6382c70b9d214946497f7736e767a4f5535c
OddMagnet <mibruenen@gmail.com>
2021-04-02 17:04:18 +0200

    Fix boids going offscreen

--- BoidsFlocking/BoidsFlocking/Boid.swift
+++ BoidsFlocking/BoidsFlocking/Boid.swift
@@ -21,10 +21,31 @@ class Boid: Identifiable {
         velocity = CGPoint(x: cos(angle), y: sin(angle))
     }
 
+    /// Calculates the new position of the boid
+    /// - Parameter flock: The flock the boid is in
     func run(in flock: Flock) {
         velocity += calculateAcceleration(with: flock)
         velocity.limit(to: maximumSpeed)
         position += velocity
+
+        // check if boid is offscreen
+        wrap(in: flock)
+    }
+
+    private func wrap(in flock: Flock) {
+        let boidSize: CGFloat = 5
+
+        if position.x < -boidSize {                     // is the boid offscreen to the left?
+            position.x = flock.width + boidSize         // then move it to the right
+        } else if position.x > flock.width + boidSize { // is the boid offscreen to the right?
+            position.x = -boidSize                      // then move it to the left
+        }
+
+        if position.y < -boidSize {                     // is the boid offscreen to the top?
+            position.y = flock.height + boidSize        // then move it to the bottom
+        } else if position.y > flock.height + boidSize {// is the boid offscreen to the bottom?
+            position.y = -boidSize                      // then move it to the top
+        }
     }
 
     // MARK: - Flocking behaviours
