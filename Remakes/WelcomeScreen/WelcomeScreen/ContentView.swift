26f45cb07ec0069e886e4543648a86ed8f4dfcc4
OddMagnet <mibruenen@gmail.com>
2022-01-09 09:30:45 +0100

    Add Scrollview for Welcome and Features text

--- Remakes/WelcomeScreen/WelcomeScreen/ContentView.swift
+++ Remakes/WelcomeScreen/WelcomeScreen/ContentView.swift
@@ -17,29 +17,33 @@ struct ContentView: View {
     ]
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Welcome to OddTracker")
-                .multilineTextAlignment(.center)
-                .font(.largeTitle.bold())
+        VStack(spacing: 20) {   // outer spacing affects small print and continue button
+            ScrollView {
+                VStack(spacing: 20) {   // inner spacing affects welcome and feature text
+                    Text("Welcome to OddTracker")
+                        .multilineTextAlignment(.center)
+                        .font(.largeTitle.bold())
 
-            ForEach(features) { feature in
-                HStack {
-                    Image(systemName: feature.image)
-                        .frame(width: 44)           // SF symbols have slightly varying sizes, ensure they're the same width
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .accessibilityHidden(true)  // hide from screen readers, since they're decorative
+                    ForEach(features) { feature in
+                        HStack {
+                            Image(systemName: feature.image)
+                                .frame(width: 44)           // SF symbols have slightly varying sizes, ensure they're the same width
+                                .font(.title)
+                                .foregroundColor(.blue)
+                                .accessibilityHidden(true)  // hide from screen readers, since they're decorative
 
-                    VStack(alignment: .leading) {
-                        Text(feature.title)
-                            .font(.headline)
+                            VStack(alignment: .leading) {
+                                Text(feature.title)
+                                    .font(.headline)
 
-                        Text(feature.description)
-                            .foregroundColor(.secondary)
+                                Text(feature.description)
+                                    .foregroundColor(.secondary)
+                            }
+                            .accessibilityElement(children: .combine)   // should be read together by screen readers
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)// ensure text is perfectly aligned, even when shorter
                     }
-                    .accessibilityElement(children: .combine)   // should be read together by screen readers
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)// ensure text is perfectly aligned, even when shorter
             }
 
             Text("Any important small print here")
